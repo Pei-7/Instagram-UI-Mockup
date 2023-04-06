a1a20492978b6428bfa71c5f514937142efee6fe
Pei Chen <peggy_chen05@live.com>
2023-04-06 23:06:18 +0800

    Initial Commit

---
+++ Instagram UI Mockup/Instagram_UI_MockupApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_UI_MockupApp.swift
+//  Instagram UI Mockup
+//
+//  Created by 陳佩琪 on 2023/4/6.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_UI_MockupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
