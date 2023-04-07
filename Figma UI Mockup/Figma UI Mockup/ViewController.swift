cac0faaa6348eeb01aac6543dfc6fb8890439ecd
Pei Chen <peggy_chen05@live.com>
2023-04-07 14:46:06 +0800

    instagram UI mockup

---
+++ Figma UI Mockup/Figma UI Mockup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Figma UI Mockup
+//
+//  Created by 陳佩琪 on 2023/4/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
